56e3368b76f880b1edb17b4dfdafa7e78b218b39
Udaykumar Narendrakumar Sharma <mr.mickeysharma@gmail.com>
2022-01-10 11:49:53 +0100

    - PRISMA-1   -- Created Project Structure and added csv file in resources

---
+++ src/test/java/com/prisma/librarymanagement/LibrarymanagementApplicationTest.java
@@ -0,0 +1,12 @@
+package com.prisma.librarymanagement;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibrarymanagementApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
